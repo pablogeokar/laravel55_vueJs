9ecbf30d780a7bc5285b9569509d82d80d87d3b0
Pablo George <pablogeokar@hotmail.com>
2018-01-21 11:21:30 -0300

    CRIAÇÃO DE COMPONENTES

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,20 +3,14 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
+        <div class="col-md-10 col-md-offset-1">
+            <!-- Componente PAINEL -->    
+            <painel titulo="Dashboard" cor="blue">
+                <div class="col-md-4">
+                <!-- Componente CAIXA -->
+                <caixa><caixa>
+                </div>                
+            </painel>
         </div>
     </div>
 </div>
